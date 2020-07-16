77df3bf4af6cecb2731f243787bb18ec38548858
VolkovVV <Volkovwladislav@mail.ru>
2020-07-16 14:40:54 +0400

    5. Tracker - хранилище [#318159]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,22 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[100];
+        int size = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (items[i].getName().equals(key)) {
+                rsl[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl,size);
     }
 
     public Item findById(int id) {
