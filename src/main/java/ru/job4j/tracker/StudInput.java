ec5da6859d5e657b51bc99aacfe1485e3d6c56fd
VolkovVV <Volkovwladislav@mail.ru>
2020-07-17 16:04:06 +0400

    3. Интерфейс Input [#318081]

---
+++ src/main/java/ru/job4j/tracker/StudInput.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class StudInput implements Input {
+    @Override
+    public String askString(String question) {
+        return null;
+    }
+
+    @Override
+    public int askInt(String question) {
+        return 0;
+    }
+}
