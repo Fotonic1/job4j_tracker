a7a4e93c893cd214e0b99301b09695b776471693
Vladislav <volkovwladislav@mail.ru>
2020-08-06 13:01:40 +0400

    Отсортировать департаменты [#318049]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] strings1 = o1.split("/");
+        String[] strings2 = o2.split("/");
+        int rsl = strings2[0].compareTo(strings1[0]);
+        if (rsl == 0 && (strings1.length > 1 || strings2.length > 1)) {
+            rsl = o1.compareTo(o2);
+        }
+        return rsl;
+    }
+}
